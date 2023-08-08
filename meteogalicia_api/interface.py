e3e55eed399f1764fb769cb17e83bf5c1f91362b
Danieldiazi <3638478+Danieldiazi@users.noreply.github.com>
2023-08-08 09:50:04 +0200

    Added station methods in order to get daily data and last 10 min of this stations

--- meteogalicia_api/interface.py
+++ meteogalicia_api/interface.py
@@ -5,7 +5,8 @@ import requests
 
 URL_FORECAST = "https://servizos.meteogalicia.gal/mgrss/predicion/jsonPredConcellos.action?idConc={}"
 URL_OBSERVATION = "https://servizos.meteogalicia.gal/mgrss/observacion/observacionConcellos.action?idConcello={}"
-
+URL_OBSERVATION_DAILYDATA_BY_STATION="https://servizos.meteogalicia.gal/mgrss/observacion/datosDiariosEstacionsMeteo.action?idEst={}"
+URL_OBSERVATION_LAST10MINDATA_BY_STATION="https://servizos.meteogalicia.gal/mgrss/observacion/ultimos10minEstacionsMeteo.action?idEst={}"
 
 
 class MeteoGalicia:
@@ -42,4 +43,14 @@ class MeteoGalicia:
              self.logger.debug(f"No observation data for {id}")
         return r
     
-
+    def get_observation_dailydata_by_station(self,id):
+        r = self._do_get(URL_OBSERVATION_DAILYDATA_BY_STATION,id)
+        if (r==None) or (not('listDatosDiarios1' in r)) or (len(r['listDatosDiarios'])==0):
+             self.logger.debug(f"No observation info (daily data) of station code: {id}")      
+        return r
+    
+    def get_observation_last10mindata_by_station(self,id):
+        r = self._do_get(URL_OBSERVATION_LAST10MINDATA_BY_STATION,id)
+        if (r==None) or (not('listUltimos10min1' in r)) or (len(r['listUltimos10min'])==0):
+             self.logger.debug(f"No observation info (last 10 min data) of station code: {id}")
+        return r
